8c72bdf5ef435e06be271d47a61629208fb4747d
riccardo <ravaro.riccardo@gmail.com>
2018-01-13 11:31:15 +0700

    crowdsale test parameters update

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -3,13 +3,13 @@ var ClaimableCrowdSale = artifacts.require("../ClaimableCrowdsale.sol");
 
 module.exports = function(deployer, network, accounts) {
   if (network === 'development') {
-    const totalSupply = web3.toWei("888888000", "Ether");
+    const totalSupply = web3.toWei("1000000000", "Ether"); // on Billion total supply
     const bankAddress = web3.eth.accounts[1];
     const beneficiaryAddress = web3.eth.accounts[2];
-    const tokenRate = 7000; // 1 ETH = 7000 AFL
-    const minBuyableAmount = web3.toWei("1", "Ether");
-    const maxTokensAmount = web3.toWei("34666632", "Ether");
-    const endDate = 1513296000;
+    const tokenRate = 7000; // 1 ETH = 7000 AFL  Public Sale
+    const minBuyableAmount = web3.toWei("0.3", "Ether");
+    const maxTokensAmount = web3.toWei("350000", "Ether");
+    const endDate = 1515987843000;
 
     deployer.deploy(AffiliateCoinToken, totalSupply).then(function () {
       return deployer.deploy(AffiliateCoinToken, totalSupply)
