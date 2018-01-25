6763a4ef2aab48a35e0d8b06ea5b13c85fbc1660
riccardo <ravaro.riccardo@gmail.com>
2018-01-25 16:18:12 +0700

    multisignWallet

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,10 +2,11 @@ var AffiliateCoinToken = artifacts.require("./AffiliateCoinToken.sol");
 
 module.exports = function(deployer, network, accounts) {
   if (network === 'cli') {
-    const totalSupply = web3.toWei("1000000000", "Ether"); // on Billion total supply
-    const bankAddress = web3.eth.accounts[1];
-    const beneficiaryAddress = web3.eth.accounts[2];
-        deployer.deploy(AffiliateCoinToken,totalSupply).then(result => {
-        })
+    const totalSupply = web3.toWei("1000000000", "Ether"); // one Billion total supply
+    deployer.deploy(AffiliateCoinToken,totalSupply).then(result => {
+
+    })
     };
 };
+
+
